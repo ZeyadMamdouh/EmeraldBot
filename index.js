0989f05437cfebb39578c455281173e47fbbdd47
ZeyadMamdouh <41742078+ZeyadMamdouh@users.noreply.github.com>
2018-07-26 22:59:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8038,6 +8038,39 @@ client.on("message", (message) => {
 
 
 
+client.on('message',  (message) => {
+            if(message.content.startsWith('-punch')) {
+      let user = message.mentions.users.first();
+      if (!user) {
+        /**
+         * The command was ran with invalid parameters.
+         * @fires commandUsage
+         */
+        return message.emit('commandUsage', message, this.help);      }
+    
+      let punches = [
+        'https://i.giphy.com/media/iWEIxgPiAq58c/giphy.gif',
+        'https://i.giphy.com/media/DViGV8rfVjw6Q/giphy.gif',
+        'https://i.giphy.com/media/GoN89WuFFqb2U/giphy.gif',
+        'https://i.giphy.com/media/xT0BKiwgIPGShJNi0g/giphy.gif',
+        'https://i.giphy.com/media/Lx8lyPHGfdNjq/giphy.gif'
+      ];
+    
+      message.channel.send({
+        embed: {
+          description: `${message.author.username} عطاك كففف ${user.username}! :punch:`,
+          image: {
+            url: punches[Math.floor(Math.random() * punches.length)]
+          }
+        }
+      }).catch(e => {
+        client.log.error(e);
+      })
+            }  
+    });
+	  
+
+
 
 
 
