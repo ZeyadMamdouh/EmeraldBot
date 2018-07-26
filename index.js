20f0682d4434aa5df8ebb5e0b92e969d87d2142f
ZeyadMamdouh <41742078+ZeyadMamdouh@users.noreply.github.com>
2018-07-26 22:19:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7990,7 +7990,7 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 });
 
 client.on("message", message => {
- if (message.content === `${prefix}help`) {
+ if (message.content === `${prefix}mhelp`) {
   const embed = new Discord.RichEmbed() //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
       .setColor("#000000")//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
       .setDescription(`
@@ -8017,6 +8017,19 @@ client.on("message", (message) => {
       if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('? ماعندك الصلاحيات');
         var member= message.mentions.members.first();
         member.kick().then((member) => {
+            message.channel.send(member.displayName + " مع السلامه :wave: ");
+        }).catch(() => {
+            message.channel.send("Error -_-");
+        });
+    }
+});
+
+
+client.on("message", (message) => {
+    if (message.content.startsWith("-ban")) {
+      if(!message.member.hasPermission('BAN_MEMBERS')) return message.reply(':warning: ماعندك الصلاحيات');
+        var member= message.mentions.members.first();
+        member.ban().then((member) => {
             message.channel.send(member.displayName + " مع السلامه :wave: ");
         }).catch(() => {
             message.channel.send("Error -_-");
