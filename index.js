0451446353c1bc27dea59fcca5000322236167ce
ZeyadMamdouh <41742078+ZeyadMamdouh@users.noreply.github.com>
2018-07-26 15:35:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -101,21 +101,7 @@ return;
 
 
 
-client.on('message', message => {
-    function resetBot(channel) {
-        // send channel a message that you're resetting bot [optional]
-        .then(msg => client.destroy())
-        .then(() => client.login(process.env.TOKEN));
-    }
-    if(message.content === adminprefix + "restart") {
-          if (!devs.includes(message.author.id)) return;
-              message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
-            console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
-            resetBot(message.channel);
-            console.log(`تم اعادة تشغيل البوت`);
-        }
-      
-      });
+
 
 
 client.on("message", message => {
