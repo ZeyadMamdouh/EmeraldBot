b0f81ba5ddad7f86cc34e90ff2dd5bafb784023a
ZeyadMamdouh <41742078+ZeyadMamdouh@users.noreply.github.com>
2018-07-27 16:12:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8692,23 +8692,7 @@ message.channel.sendMessage({embed: {
 
 
 
-  var AsciiTable = require('ascii-data-table').default
-client.on('message', message =>{
-
-    if(message.content == "-roles"){
-        var 
-        ros=message.guild.roles.size,
-        data = [['Rank', 'RoleName']]
-        for(let i =0;i<ros;i++){
-            if(message.guild.roles.array()[i].id !== message.guild.id){
-         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
-        }}
-        let res = AsciiTable.table(data)
-
-        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
-    }
-});
-  
+ 
 
 
 
