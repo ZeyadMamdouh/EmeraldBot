e423698e639e865b0b973b12aaa3404547ae8514
ZeyadMamdouh <41742078+ZeyadMamdouh@users.noreply.github.com>
2018-07-27 19:01:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8669,6 +8669,25 @@ function play(guild, song) {
 
 	serverQueue.textChannel.send(`ًںژ¶ Start playing: **${song.title}**`);
 }
+client.on("message", message => {
+ if (message.content === `${prefix}help`) {
+  const embed = new Discord.RichEmbed()
+      .setColor("#000000")
+      .setDescription(`
+${prefix}play ⇏ لتشغيل أغنية برآبط أو بأسم
+${prefix}skip ⇏ لتجآوز الأغنية الحآلية
+${prefix}pause ⇏ إيقآف الأغنية مؤقتا
+${prefix}resume ⇏ لموآصلة الإغنية بعد إيقآفهآ مؤقتا
+${prefix}volume ⇏ لتغيير درجة الصوت 100 - 0
+${prefix}stop ⇏ لإخرآج البوت من الروم
+${prefix}np ⇏ لمعرفة الأغنية المشغلة حآليا
+${prefix}queue ⇏ لمعرفة قآئمة التشغيل
+
+ `)
+   message.channel.sendEmbed(embed)
+    
+   }
+   }); 
 
 
 
