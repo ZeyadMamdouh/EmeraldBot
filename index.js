82134699f876f7a80405529fcdac2ffe7002b454
ZeyadMamdouh <41742078+ZeyadMamdouh@users.noreply.github.com>
2018-07-31 16:00:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8754,6 +8754,41 @@ m.sendMessage(args)
 
 
 
+ client.on('message', message => {
+  if (message.author.codes) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**انت لا تملك الصلاحيات المطلوبه**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply(" **يجب تحديد شخص** KICK_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  
+  if (message.mentions.users.size < 1) return message.reply("**يجب تحديد شخص**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**");
+
+
+  message.guild.member(user).ban(7, user);
+
+message.channel.send(`**:white_check_mark: ${user.tag} banned from the server ! :airplane: **  `)
+
+}
+});
+  
+
+
+
+
+
+
+
 
 
 
